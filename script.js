00a79024c2acdc47e11c3c2e3b31d6420a4d39c2
Jan Steikowski <j.steikowski@aol.com>
2022-11-06 20:23:02 +0700

    Add function to receive User Input

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
+//Create Array and store the values Rock Paper and Sissor in it
+
 let computerChoice = [];
 computerChoice[0] = 'Rock';
 computerChoice[1] = 'Paper';
 computerChoice[2] = 'Sissor';
 
+//generate a random number
 function calcRandomNumber() {
     return Math.floor(Math.random() * 3);
 }
-
+//choose a random Array element
 function getComputerChoice() {
     return computerChoice[calcRandomNumber()];
 }
-
+//register userInput
+function getUserInput() {
+    let userInput = prompt('Please choose between Rock, paper, or Sissor:').toLowerCase();
+    return userInput;
+}
+//check if userInput is either Rock, Paper or Sissor
