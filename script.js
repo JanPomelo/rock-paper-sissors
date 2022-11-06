ee285dbd458478c6911583de68b3f3d4c67ab894
Jan Steikowski <j.steikowski@aol.com>
2022-11-06 19:54:00 +0700

    Add array with ComputerChoice options

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
+let computerChoice = [];
+computerChoice[0] = 'Rock';
+computerChoice[1] = 'Paper';
+computerChoice[2] = 'Sissor';
 
 function calcRandomNumber() {
     return Math.floor(Math.random() * 3);
 }
+
+
