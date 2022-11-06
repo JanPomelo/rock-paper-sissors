de878e5129d5fd827bec1355bd6592f14b9e8db9
Jan Steikowski <j.steikowski@aol.com>
2022-11-06 20:26:08 +0700

    Add function to check if userInput is valid
    If the user types different words than rock, paper or sissor (in all VaRiAtIoNs), the function will return false

--- script.js
+++ script.js
@@ -18,4 +18,11 @@ function getUserInput() {
     let userInput = prompt('Please choose between Rock, paper, or Sissor:').toLowerCase();
     return userInput;
 }
+
 //check if userInput is either Rock, Paper or Sissor
+function checkUserInput(userInput) {
+    if (userInput === 'rock' || userInput === 'paper' || userInput === 'sissor') {
+        return true;
+    }
+    else return false;
+}
